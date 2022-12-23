9c0aaf394ff7e0d47f0b1ea2c72488f5f8483ed9
intrandom5 <random_5@naver.com>
2022-12-23 09:42:50 +0000

    fix: fix util for retriever

--- retriever/dataset/utils.py
+++ retriever/dataset/utils.py
@@ -4,90 +4,55 @@ class Preprocess_features:
         self.max_length = max_length
         self.stride = stride
 
-       
-    def prepare_train_features(self, examples):
+    def prepare_features(self, examples):
         tokenized_examples = self.tokenizer(
-            examples["question"],
             examples["context"],
-            truncation="only_second",
+            truncation=True,
             max_length=self.max_length,
             stride=self.stride,
             return_overflowing_tokens=True,
             return_offsets_mapping=True,
             padding="max_length",
         )
-
-        overflow_to_sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
-        offset_mapping = tokenized_examples.pop("offset_mapping")
+        truncated = {"input_ids": [], "token_type_ids": [], "attention_mask": [], "start_index": [], "end_index": []}
+        
+        overflow_to_sample_mapping = tokenized_examples["overflow_to_sample_mapping"]
+        offset_mapping = tokenized_examples["offset_mapping"]
 
         tokenized_examples["start_positions"] = []
         tokenized_examples["end_positions"] = []
-
+        
+        print("truncating datasets...")
         for i, offsets in enumerate(offset_mapping):
             input_ids = tokenized_examples["input_ids"][i]
-            cls_index = input_ids.index(self.tokenizer.cls_token_id)
 
             sequence_ids = tokenized_examples.sequence_ids(i)
-            
+
             example_index = overflow_to_sample_mapping[i]
             answers = examples["answers"][example_index]
-            
+
             answer_start_offset = answers["answer_start"][0]
             answer_end_offset = answer_start_offset + len(answers["text"][0])
 
             token_start_index = 0
-            while sequence_ids[token_start_index] != 1:
+            while sequence_ids[token_start_index] != 0:
                 token_start_index += 1
-            
+
             token_end_index = len(input_ids) - 1
-            while sequence_ids[token_end_index] != 1:
+            while sequence_ids[token_end_index] != 0:
                 token_end_index -= 1
 
-            if not (
+            if (
                 offsets[token_start_index][0] <= answer_start_offset
                 and offsets[token_end_index][1] >= answer_end_offset
             ):
-                tokenized_examples["start_positions"].append(cls_index)
-                tokenized_examples["end_positions"].append(cls_index)
-            else:
-                while (
-                    token_start_index < len(offsets)
-                    and offsets[token_start_index][0] <= answer_start_offset
-                ):
-                    token_start_index += 1
-                tokenized_examples["start_positions"].append(token_start_index - 1)
-                while offsets[token_end_index][1] >= answer_end_offset:
-                    token_end_index -= 1
-                tokenized_examples["end_positions"].append(token_end_index + 1)
-
-        return tokenized_examples
+                truncated["input_ids"].append(tokenized_examples["input_ids"][i])
+                truncated["attention_mask"].append(tokenized_examples["attention_mask"][i])
+                truncated["token_type_ids"].append(tokenized_examples["token_type_ids"][i])
+                truncated["start_index"].append(token_start_index)
+                truncated["end_index"].append(token_end_index)
+        
+        print("truncating completed!")
+        
+        return truncated
     
-    def prepare_validation_features(self, examples):
-        tokenized_examples = self.tokenizer(
-            examples["question"],
-            examples["context"],
-            truncation="only_second",
-            max_length=self.max_length,
-            stride=self.stride,
-            return_overflowing_tokens=True,
-            return_offsets_mapping=True,
-            padding="max_length",
-        )
-
-        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
-
-        tokenized_examples["example_id"] = []
-
-        for i in range(len(tokenized_examples["input_ids"])):
-            sequence_ids = tokenized_examples.sequence_ids(i)
-            context_index = 1
-
-            sample_index = sample_mapping[i]
-            tokenized_examples["example_id"].append(examples["id"][sample_index])
-
-            tokenized_examples["offset_mapping"][i] = [
-                (o if sequence_ids[k] == context_index else None)
-                for k, o in enumerate(tokenized_examples["offset_mapping"][i])
-            ]
-
-        return tokenized_examples
