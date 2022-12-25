34eb6f751a6fdd58251494cffad1fcee1d625870
intrandom5 <random_5@naver.com>
2022-12-25 05:40:05 +0000

    fix: finally fixed utils Process_features...

--- retriever/dataset/utils.py
+++ retriever/dataset/utils.py
@@ -4,9 +4,10 @@ class Preprocess_features:
         self.max_length = max_length
         self.stride = stride
 
-    def prepare_features(self, examples):
-        tokenized_examples = self.tokenizer(
-            examples["context"],
+    def process(self, train_dataset):
+        contexts = [context.replace("\\n", "") for context in train_dataset["context"]]
+        tokenized_contexts = self.tokenizer(
+            contexts,
             truncation=True,
             max_length=self.max_length,
             stride=self.stride,
@@ -14,22 +15,22 @@ class Preprocess_features:
             return_offsets_mapping=True,
             padding="max_length",
         )
-        truncated = {"input_ids": [], "token_type_ids": [], "attention_mask": [], "start_index": [], "end_index": []}
-        
-        overflow_to_sample_mapping = tokenized_examples["overflow_to_sample_mapping"]
-        offset_mapping = tokenized_examples["offset_mapping"]
 
-        tokenized_examples["start_positions"] = []
-        tokenized_examples["end_positions"] = []
-        
-        print("truncating datasets...")
+        overflow_to_sample_mapping = tokenized_contexts.pop("overflow_to_sample_mapping")
+        offset_mapping = tokenized_contexts.pop("offset_mapping")
+
+        tokenized_contexts["start_positions"] = []
+        tokenized_contexts["end_positions"] = []
+        tokenized_contexts["questions"] = []
+        tokenized_contexts["answers"] = []
+
         for i, offsets in enumerate(offset_mapping):
-            input_ids = tokenized_examples["input_ids"][i]
+            input_ids = tokenized_contexts["input_ids"][i]
 
-            sequence_ids = tokenized_examples.sequence_ids(i)
+            sequence_ids = tokenized_contexts.sequence_ids(i)
 
             example_index = overflow_to_sample_mapping[i]
-            answers = examples["answers"][example_index]
+            answers = train_dataset["answers"][example_index]
 
             answer_start_offset = answers["answer_start"][0]
             answer_end_offset = answer_start_offset + len(answers["text"][0])
@@ -42,17 +43,27 @@ class Preprocess_features:
             while sequence_ids[token_end_index] != 0:
                 token_end_index -= 1
 
-            if (
+            if not (
                 offsets[token_start_index][0] <= answer_start_offset
                 and offsets[token_end_index][1] >= answer_end_offset
             ):
-                truncated["input_ids"].append(tokenized_examples["input_ids"][i])
-                truncated["attention_mask"].append(tokenized_examples["attention_mask"][i])
-                truncated["token_type_ids"].append(tokenized_examples["token_type_ids"][i])
-                truncated["start_index"].append(token_start_index)
-                truncated["end_index"].append(token_end_index)
+                tokenized_contexts["start_positions"].append(-1)
+                tokenized_contexts["end_positions"].append(-1)
+                tokenized_contexts["questions"].append(None)
+                tokenized_contexts["answers"].append(None)
+            else:
+                while (
+                    token_start_index < len(offsets)
+                    and offsets[token_start_index][0] <= answer_start_offset
+                ):
+                    token_start_index += 1
+                while offsets[token_end_index][1] >= answer_end_offset:
+                    token_end_index -= 1
+                
+                tokenized_contexts["start_positions"].append(token_start_index)
+                tokenized_contexts["end_positions"].append(token_end_index)
+                tokenized_contexts["questions"].append(train_dataset["question"][example_index])
+                tokenized_contexts["answers"].append(train_dataset["answers"][example_index])
+                
+        return tokenized_contexts
         
-        print("truncating completed!")
-        
-        return truncated
-    
