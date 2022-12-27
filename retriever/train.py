8180b32bf357579e13d64a955f975d84f2f3d16c
intrandom5 <random_5@naver.com>
2022-12-27 02:07:58 +0000

    fix: get WikiDataset in train.py

--- retriever/train.py
+++ retriever/train.py
@@ -1,4 +1,4 @@
-from dataset import RetrieverDataset
+from dataset import RetrieverDataset, WikiDataset
 from model import DenseRetriever
 
 import torch
@@ -46,17 +46,20 @@ class RetrieverTrainer:
 
         self.num_neg = self.config["num_negative_passages_per_question"]
         
-        wiki_contexts = pd.read_csv(config["corpus_path"])["text"]
-        self.wiki_contexts = [context.replace("\\n", "") for context in wiki_contexts]
-        self.tokenized_corpus = self.train_datasets.tokenizer(
-            self.wiki_contexts,
-            truncation=True,
-            stride=config["stride"],
-            return_overflowing_tokens=True,
-            return_offsets_mapping=True,
-            padding="max_length",
-            return_tensors="pt"
-        )
+        self.wikidataset = WikiDataset(config=config, tokenizer=self.train_datasets.tokenizer)
+        self.wikiloader = DataLoader(self.wikidataset, batch_size=16, shuffle=False)
+        
+        # wiki_contexts = pd.read_csv(config["corpus_path"])["text"]
+        # self.wiki_contexts = [context.replace("\\n", "") for context in wiki_contexts]
+        # self.tokenized_corpus = self.train_datasets.tokenizer(
+        #     self.wiki_contexts,
+        #     truncation=True,
+        #     stride=config["stride"],
+        #     return_overflowing_tokens=True,
+        #     return_offsets_mapping=True,
+        #     padding="max_length",
+        #     return_tensors="pt"
+        # )
 
 
     def train(self):
