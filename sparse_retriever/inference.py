0bf87107013f4e3380da66f1b9c2cd32adaf3006
intrandom5 <random_5@naver.com>
2023-01-01 22:33:12 +0000

    feat: add sparse retriever

---
+++ sparse_retriever/inference.py
@@ -0,0 +1,51 @@
+from transformers import AutoTokenizer
+from datasets import load_from_disk
+from tf_idf import TfIdfRetrieval
+from BM25 import BM25Retriever
+import pandas as pd
+
+
+def main():
+    corpus_path = "../../../ODQA/data/wikipedia_documents.json"
+    pickle_path = "tf-idf_wiki.pickle"
+    tokenizer_name = "klue/bert-base"
+    validation_path = "../../../ODQA/data/train_dataset"
+    result_path = "tf-idf_valid_result.csv"
+    sparse = "tf-idf"
+    top_k = 3
+    assert sparse in ["BM25", "tf-idf"], print("sparse must be in ['BM25', 'tf-idf']")
+    
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    tokenize_fn = tokenizer.tokenize
+    
+    valid_data = load_from_disk(validation_path)["validation"]
+    questions = valid_data["question"]
+    
+    if sparse == "BM25":
+        print("load BM25Retriever...")
+        SparseRetrieval = BM25Retriever(tokenize_fn, corpus_path, pickle_path)
+        SparseRetrieval.get_sparse_embedding()
+        print(f"get top-{top_k} relevant documents...")
+        docs = SparseRetrieval.get_relevant_doc(query=questions, k=top_k)
+    elif sparse == "tf-idf":
+        print("load TfIdfRetriever...")
+        SparseRetrieval = TfIdfRetrieval(tokenize_fn, corpus_path, pickle_path)
+        SparseRetrieval.get_sparse_embedding()
+        print(f"get top-{top_k} relevant documents...")
+        docs_scores, docs = SparseRetrieval.get_relevant_doc(query=questions, k=top_k)
+    
+    df_dict = {"question_id": [], "question": [], "subdocument": []}
+    
+    for i, result in enumerate(docs):
+        for doc in result:
+            df_dict["question_id"].append(i)
+            df_dict["question"].append(valid_data["question"][i])
+            df_dict["subdocument"].append(doc)
+            
+    df = pd.DataFrame.from_dict(df_dict)
+    df.to_csv(result_path, index=False)
+    
+    
+if __name__=="__main__":
+    main()
+    
