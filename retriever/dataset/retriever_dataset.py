43b7e66daaccf08c4e6dcdce6080f70b7ebdc1a3
intrandom5 <random_5@naver.com>
2023-01-03 04:15:01 +0000

    remove augmentedRetrieverDataset in retriever_dataset.py

--- retriever/dataset/retriever_dataset.py
+++ retriever/dataset/retriever_dataset.py
@@ -3,7 +3,7 @@ import datasets
 import numpy as np
 import pandas as pd
 from typing import List
-from datasets import load_from_disk
+from datasets import load_from_disk, load_dataset
 from torch.utils.data import Dataset
 from .utils import Preprocess_features
 from transformers import AutoTokenizer
@@ -27,7 +27,8 @@ class RetrieverDataset(Dataset):
             "train",
             "validation",
             "test",
-        ], f"RetrieverDataset > __init__: The mode {mode} does not exist. This must be one of these: 'train', 'validation', or 'test'."
+            "korquad",
+        ], f"RetrieverDataset > __init__: The mode {mode} does not exist. This must be one of these: 'train', 'validation', 'test' or 'korquad'."
         super(RetrieverDataset, self).__init__()
         self.config = config
         self.mode = mode
@@ -41,11 +42,14 @@ class RetrieverDataset(Dataset):
 
         self.PE = Preprocess_features(self.tokenizer, self.max_length, self.stride)
 
-        if mode == "train":
+        if mode in ["train", "korquad"]:
             print(
                 "RetrieverDataset > __init__: You are currently in the TRAINING process. It will construct in-batch negative samples."
             )
-            self.dataset = load_from_disk(config["train_data_path"])["train"]
+            if mode == "korquad":
+                self.dataset = load_dataset("squad_kor_v1")
+            else:
+                self.dataset = load_from_disk(config["train_data_path"])["train"]
             (
                 self.tokenized_passages,
                 self.tokenized_questions,
@@ -152,65 +156,6 @@ class HardNegatives(Dataset):
             torch.tensor(self.tokenized_hard_negatives['token_type_ids'][index])
         ]
         return items
-
-
-class AugmentedRetrieverDataset(Dataset):
-    def __init__(self, config):
-        super(AugmentedRetrieverDataset, self).__init__()
-        self.config = config
-        self.dataset = load_from_disk(config["augmented_train_data_path"])["train"]
-        self.tokenizer = AutoTokenizer.from_pretrained(config["model_name_or_path"])
-        self.max_length = self.config["max_length"]
-        self.stride = self.config["stride"]
-        self.PE = Preprocess_features(self.tokenizer, self.max_length, self.stride)
-        (
-            self.tokenized_passages,
-            self.tokenized_questions,
-        ) = self.construct_in_batch_negative_sampled_dataset()
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, index):
-        return (
-                self.tokenized_passages["input_ids"][index],
-                self.tokenized_passages["attention_mask"][index],
-                self.tokenized_passages["token_type_ids"][index],
-                self.tokenized_questions["input_ids"][index],
-                self.tokenized_questions["attention_mask"][index],
-                self.tokenized_questions["token_type_ids"][index],
-            )
-
-    def construct_in_batch_negative_sampled_dataset(self):
-        column_names = self.dataset.column_names
-        tokenized_passages = self.dataset.map(
-            self.PE.process,
-            batched=True,
-            num_proc=4,
-            remove_columns=column_names,
-            load_from_cache_file=True,
-        )
-        questions = tokenized_passages["questions"]
-
-        Passages = {"input_ids": [], "attention_mask": [], "token_type_ids": []}
-        Questions = []
-        Answers = []
-
-        for passage, question in zip(tokenized_passages, questions):
-            if question != None:
-                Passages["input_ids"].append(passage["input_ids"])
-                Passages["attention_mask"].append(passage["attention_mask"])
-                Passages["token_type_ids"].append(passage["token_type_ids"])
-                Answers.append(passage["answers"])
-                Questions.append(question)
-
-        tokenized_questions = self.tokenizer(
-            Questions, padding=True, return_tensors="pt"
-        )
-
-        tokenized_passages = {k: torch.tensor(v) for k, v in Passages.items()}
-
-        return tokenized_passages, tokenized_questions
 
 
 class WikiDataset(Dataset):
