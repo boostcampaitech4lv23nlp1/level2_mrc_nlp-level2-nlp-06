142f817e737326d1d02ed230734a2ac0ba20bd2d
intrandom5 <random_5@naver.com>
2023-01-03 04:31:50 +0000

    fix: remove TODO in train.py

--- retriever/train.py
+++ retriever/train.py
@@ -201,7 +201,6 @@ class RetrieverTrainer:
 
 
     def save_checkpoint(self, epoch, valid_loss):
-        ## TODO: save_path로 디렉토리를 받도록 수정하기. 이를 위해선 inference 코드들이 수정되어야 함. -> inference용 config 만들기.
         torch.save(self.p_encoder.state_dict(), f"{self.config['p_encoder_save_path'][:-3]}-{epoch}-{valid_loss:.6f}.pt")
 
 
